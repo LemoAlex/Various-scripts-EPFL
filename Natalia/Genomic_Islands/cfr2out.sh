e65ccbb7ee31cb5b91aab24f43f39343991e51d5
Alex Lemo <61739499+LemoAlex@users.noreply.github.com>
2024-12-13 11:30:16 +0100

    Delete Natalia directory

--- Natalia/Genomic_Islands/cfr2out.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in *.gff
-do
-grep 'PUTAL' $i | tail -n +2 |cut -f 3,4,5| sed 's/\t/../g'| sed 's/PUTAL../Misc\t/g' | sed 's/Misc/ \t\/note="SigCRF" \n Misc/g'| tail -n +2 | sed '$s/$/\n \t\/note="SigCRF"/' > $i.CRF.out 
-done
-
